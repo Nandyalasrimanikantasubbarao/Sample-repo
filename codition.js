2af3f06eab2b0748677e47587b6734b4d51180e7
Nandyalasrimanikantasubbarao <srimaninandyala@gmail.com>
2023-03-30 14:11:31 +0530

    commit3

---
+++ codition.js
@@ -0,0 +1,51 @@
+//if else
+var num1=10
+var num2=20
+if(num1>num2){
+    console.log("1st num is bigger")
+}
+else{
+    console.log("2nd num is bigger")
+}
+//if
+var a=1;
+if(a>0){
+    console.log("positive");
+}
+if(a<0){
+    console.log("negative");
+}
+if(a>=0){
+    console.log("it real number");
+}
+//Nested if else
+var a=1;
+if(a>=0){
+    if(a>0){
+        console.log("+ve");
+    }
+    else{
+        console.log("zero");
+    }
+}
+else{
+    console.log("-ve");
+}
+
+//switch
+var num1=20;
+var num2=30;
+switch(num1>um2){
+    case true:
+        console.log("this is first case");
+        break;
+
+     case false:
+        console.log("this is second case");
+        break;
+     
+    default:
+        console.log("nothing");
+
+     
+}
--- myfirst.js
+++ myfirst.js
@@ -1,1 +1,19 @@
-console.log("Hello, This code is from my html file");
+var name="manikanta";
+var age="19";
+const okay="true"
+console.log(okay);
+console.log(name);
+console.log(age);
+var one=1;
+var two=2.2;
+var three=(one+two)
+console.log(three)
+var num=[1,2,3,4,5,100];//Array
+console.log(num);
+var obj ={
+
+name:"manikanta",
+id:25,
+test: ["fail", "pass", "fail"],
+}
+console.log(obj);
